1fdbf445e868a49009d9a8701b8246c516508081
demonlord1997 <2273893094@qq.com>
2020-06-15 20:05:09 +0800

    None

--- pdf2img.py
+++ pdf2img.py
@@ -7,21 +7,15 @@
 ##########################################################################
 import tempfile
 import argparse
-import os
 from pdf2image import convert_from_path,convert_from_bytes
  
-#以下三种方式都可以读取文件，第三种最好
-# image = convert_from_path("./")
-
-# image = convert_from_bytes(open('./csp2nn.pdf', 'rb').read())
-# inputfile = input()
-# outfile = input()
 parser = argparse.ArgumentParser(description='pdf2image')
 parser.add_argument('--inputfile', '-i', help='input your pdf file',required=True)
+parser.add_argument('--dpi', '-d', help='input your image dpi',default=400)
 parser.add_argument('--outfolder', '-f', help='set your image\'s path', required=True)
 parser.add_argument('--outfile', '-o', help='set your image\'s name', default="out")
 args = parser.parse_args()
 
 with tempfile.TemporaryDirectory() as path:
-    image_from_path = convert_from_path(args.inputfile, dpi=400, fmt='png',output_folder=args.outfolder,output_file=args.outfile)
+    image_from_path = convert_from_path(args.inputfile, dpi=args.dpi, fmt='png',output_folder=args.outfolder,output_file=args.outfile)
 
