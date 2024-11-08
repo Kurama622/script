9286514f45807bf49ac9f1f55ec4f5a5bde899d7
Kurama <kurama174@163.com>
2024-11-09 00:29:42 +0800

    update network

--- status.sh
+++ status.sh
@@ -5,38 +5,39 @@
 # This function parses /proc/net/dev file searching for a line containing $interface data.
 # Within that line, the first and ninth numbers after ':' are respectively the received and transmited bytes.
 #function get_bytes {
-    ## Find active network interface
-    #interface=$(ip route get 8.8.8.8 2>/dev/null| awk '{print $5}')
-    #line=$(grep $interface /proc/net/dev | cut -d ':' -f 2 | awk '{print "received_bytes="$1, "transmitted_bytes="$9}')
-    #eval $line
-    #now=$(date +%s%N)
+## Find active network interface
+#interface=$(ip route get 8.8.8.8 2>/dev/null| awk '{print $5}')
+#line=$(grep $interface /proc/net/dev | cut -d ':' -f 2 | awk '{print "received_bytes="$1, "transmitted_bytes="$9}')
+#eval $line
+#now=$(date +%s%N)
 #}
 
-dwm_network(){
-    CONNAME=$(nmcli -a | grep '已连接' | awk 'NR==1{print $4}')
-    if [ "$IDENTIFIER" = "unicode" ]; then
-        echo "🌐 $CONNAME"
-    else
-        echo "NET $CONNAME"
-    fi
+dwm_network() {
+  CONNAME=$(nmcli -a | grep '已连接' | awk 'NR==1{print $4}')
+  if [ "$IDENTIFIER" = "unicode" ]; then
+    echo "🌐 $CONNAME"
+  else
+    echo "NET $CONNAME"
+  fi
 }
+
 # Function which calculates the speed using actual and old byte number.
 # Speed is shown in KByte per second when greater or equal than 1 KByte per second.
 # This function should be called each second.
 
 #function get_velocity {
-    #value=$1
-    #old_value=$2
-    #now=$3
+#value=$1
+#old_value=$2
+#now=$3
 
-    #timediff=$(($now - $old_time))
-    #velKB=$(echo "1000000000*($value-$old_value)/1024/$timediff" | bc)
-    #if test "$velKB" -gt 1024
-    #then
-        #echo $(echo "scale=2; $velKB/1024" | bc)MB/s
-    #else
-        #echo ${velKB}KB/s
-    #fi
+#timediff=$(($now - $old_time))
+#velKB=$(echo "1000000000*($value-$old_value)/1024/$timediff" | bc)
+#if test "$velKB" -gt 1024
+#then
+#echo $(echo "scale=2; $velKB/1024" | bc)MB/s
+#else
+#echo ${velKB}KB/s
+#fi
 #}
 
 # Get initial values
@@ -46,127 +47,129 @@ dwm_network(){
 #old_time=$now
 
 print_volume() {
-    volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
-    if test "$volume" -gt 0
-    then
-        echo -e "\uE05D${volume}"
-    else
-        echo -e "Mute"
-    fi
+  volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
+  if test "$volume" -gt 0; then
+    echo -e "\uE05D${volume}"
+  else
+    echo -e "Mute"
+  fi
 }
 
-print_mem(){
-    memfree=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
-    echo -e "\x04[${memfree}M] "
+print_network() {
+  network_name=$(nmcli | grep 'connected to ' | awk -F 'connected to ' '{print $2}')
+  echo -e " \x01(${network_name}) "
 }
 
-print_temp(){
-    test -f /sys/class/thermal/thermal_zone0/temp || return 0
-    echo $(head -c 2 /sys/class/thermal/thermal_zone0/temp)C
+print_mem() {
+  memfree=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
+  echo -e "\x04[${memfree}M] "
+}
+
+print_temp() {
+  test -f /sys/class/thermal/thermal_zone0/temp || return 0
+  echo $(head -c 2 /sys/class/thermal/thermal_zone0/temp)C
 }
 
 #!/bin/bash
 
 #get_time_until_charged() {
 
-    #sum_remaining_charge=$(acpitool -B | grep -E 'Last full capacity' | awk '{print $5}' | grep -Eo "[0-9]+" | paste -sd+ | bc);
+#sum_remaining_charge=$(acpitool -B | grep -E 'Last full capacity' | awk '{print $5}' | grep -Eo "[0-9]+" | paste -sd+ | bc);
 
-    #sum_remaining_charge_percent=$(acpitool -B | grep -E 'Remaining capacity' | awk '{print $5}' | grep -Eo "[0-9.]+" | paste -sd+ | bc);
+#sum_remaining_charge_percent=$(acpitool -B | grep -E 'Remaining capacity' | awk '{print $5}' | grep -Eo "[0-9.]+" | paste -sd+ | bc);
 
-    #sum_remaining_charge=$sum_remaining_charge*$sum_remaining_charge_percent/100;
-    ## finds the rate at which the batteries being drained at
-    #present_rate=$(acpitool -B | grep -E 'Present rate' | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc);
+#sum_remaining_charge=$sum_remaining_charge*$sum_remaining_charge_percent/100;
+## finds the rate at which the batteries being drained at
+#present_rate=$(acpitool -B | grep -E 'Present rate' | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc);
 
-    ## divides current charge by the rate at which it's falling, then converts it into seconds for `date`
-    #seconds=$(bc <<< "scale = 10; ($sum_remaining_charge / $present_rate) * 3600");
+## divides current charge by the rate at which it's falling, then converts it into seconds for `date`
+#seconds=$(bc <<< "scale = 10; ($sum_remaining_charge / $present_rate) * 3600");
 
-    ## prettifies the seconds into h:mm:ss format
-    #pretty_time=$(date -u -d @${seconds} +%T);
+## prettifies the seconds into h:mm:ss format
+#pretty_time=$(date -u -d @${seconds} +%T);
 
-    #echo $pretty_time;
+#echo $pretty_time;
 #}
 
 get_battery_combined_percent() {
 
-    # get charge of all batteries, combine them
-    total_charge=$(expr $(acpi -b | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc));
+  # get charge of all batteries, combine them
+  total_charge=$(expr $(acpi -b | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc))
 
-    # get amount of batteries in the device
-    battery_number=$(acpi -b | wc -l);
+  # get amount of batteries in the device
+  battery_number=$(acpi -b | wc -l)
 
-    percent=$(expr $total_charge / $battery_number);
+  percent=$(expr $total_charge / $battery_number)
 
-    echo $percent;
+  echo $percent
 }
 
 get_battery_charging_status() {
 
-    if $(acpi -b | grep --quiet Discharging)
-    then
-        echo "🔋";
-    else # acpi can give Unknown or Charging if charging, https://unix.stackexchange.com/questions/203741/lenovo-t440s-battery-status-unknown-but-charging
-        echo "🔌";
-    fi
+  if $(acpi -b | grep --quiet Discharging); then
+    echo "🔋"
+  else # acpi can give Unknown or Charging if charging, https://unix.stackexchange.com/questions/203741/lenovo-t440s-battery-status-unknown-but-charging
+    echo "🔌"
+  fi
 }
 
-
-print_bat(){
-    #hash acpi || return 0
-    #onl="$(grep "on-line" <(acpi -V))"
-    #charge="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)%"
-    #if test -z "$onl"
-    #then
-        ## suspend when we close the lid
-        ##systemctl --user stop inhibit-lid-sleep-on-battery.service
-        #echo -e "${charge}"
-    #else
-        ## On mains! no need to suspend
-        ##systemctl --user start inhibit-lid-sleep-on-battery.service
-        #echo -e "${charge}"
-    #fi
-    #echo "$(get_battery_charging_status) $(get_battery_combined_percent)%, $(get_time_until_charged )";
-    echo "$(get_battery_charging_status)$(get_battery_combined_percent)%";
+print_bat() {
+  #hash acpi || return 0
+  #onl="$(grep "on-line" <(acpi -V))"
+  #charge="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)%"
+  #if test -z "$onl"
+  #then
+  ## suspend when we close the lid
+  ##systemctl --user stop inhibit-lid-sleep-on-battery.service
+  #echo -e "${charge}"
+  #else
+  ## On mains! no need to suspend
+  ##systemctl --user start inhibit-lid-sleep-on-battery.service
+  #echo -e "${charge}"
+  #fi
+  #echo "$(get_battery_charging_status) $(get_battery_combined_percent)%, $(get_time_until_charged )";
+  echo "$(get_battery_charging_status)$(get_battery_combined_percent)%"
 }
 
-print_date(){
-    #echo '📆 '; date '+%Y-%m-%d | %H:%M'
-    # printf "📆 %s" "$(date "+%a %y-%m-%d $%T")"
-    # printf "\x03%s" "$(date "+[%a %y-%m-%d]——[%T]")"
-    echo -e "\x03$(date '+[%a %y-%m-%d]——[%T]')"
+print_date() {
+  #echo '📆 '; date '+%Y-%m-%d | %H:%M'
+  # printf "📆 %s" "$(date "+%a %y-%m-%d $%T")"
+  # printf "\x03%s" "$(date "+[%a %y-%m-%d]——[%T]")"
+  echo -e "\x03$(date '+[%a %y-%m-%d]——[%T]')"
 }
 
-show_record(){
-    test -f /tmp/r2d2 || return
-    rp=$(cat /tmp/r2d2 | awk '{print $2}')
-    size=$(du -h $rp | awk '{print $1}')
-    echo " $size $(basename $rp)"
+show_record() {
+  test -f /tmp/r2d2 || return
+  rp=$(cat /tmp/r2d2 | awk '{print $2}')
+  size=$(du -h $rp | awk '{print $1}')
+  echo " $size $(basename $rp) "
 }
 
-dwm_alsa () {
-    VOL=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
-    printf "%s" "$SEP1"
-    if [ "$IDENTIFIER" = "unicode" ]; then
-        if [ "$VOL" -eq 0 ]; then
-            printf "🔇"
-        elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
-            printf "🔈 %s%%" "$VOL"
-        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
-            printf "🔉 %s%%" "$VOL"
-        else
-            printf "🔊 %s%%" "$VOL"
-        fi
+dwm_alsa() {
+  VOL=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
+  printf "%s" "$SEP1"
+  if [ "$IDENTIFIER" = "unicode" ]; then
+    if [ "$VOL" -eq 0 ]; then
+      printf "🔇"
+    elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+      printf "🔈 %s%%" "$VOL"
+    elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+      printf "🔉 %s%%" "$VOL"
     else
-        if [ "$VOL" -eq 0 ]; then
-            printf "MUTE"
-        elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
-            printf "VOL %s%%" "$VOL"
-        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
-            printf "VOL %s%%" "$VOL"
-        else
-            printf "VOL %s%%" "$VOL"
-        fi
+      printf "🔊 %s%%" "$VOL"
     fi
-    printf "%s\n" "$SEP2"
+  else
+    if [ "$VOL" -eq 0 ]; then
+      printf "MUTE"
+    elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+      printf "VOL %s%%" "$VOL"
+    elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+      printf "VOL %s%%" "$VOL"
+    else
+      printf "VOL %s%%" "$VOL"
+    fi
+  fi
+  printf "%s\n" "$SEP2"
 }
 
 LOC=$(readlink -f "$0")
@@ -196,7 +199,7 @@ export IDENTIFIER="unicode"
 #xsetroot -name " 💿$(print_mem)G | $(dwm_network) ⬇️ $vel_recv ⬆️ $vel_trans $(dwm_alsa) [$(print_bat)]$(show_record) $(print_date) "
 
 # xsetroot -name " $(print_mem)$(print_space)$(print_date)"
-xsetroot -name " $(print_mem)$(print_date)"
+xsetroot -name "$(print_network)$(print_mem)$(print_date)"
 
 # Update old values to perform new calculations
 #old_received_bytes=$received_bytes
